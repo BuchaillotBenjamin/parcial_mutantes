fe25460e67062e72284faf76e46ba67f2562100c
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2024-10-03 11:16:00 -0300

    primer commit

---
+++ src/test/java/com/example/parcial_mutantes/ParcialMutantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcial_mutantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialMutantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
