53c1ae51211ec415a7691ec2bfca654e527f7a39
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2024-10-10 13:26:41 -0300

    separo en metodos la busqueda

--- src/main/java/com/example/parcial_mutantes/service/MutantesService.java
+++ src/main/java/com/example/parcial_mutantes/service/MutantesService.java
@@ -41,18 +41,35 @@ public class MutantesService {
         final int secuenciaMinima = 4;
         int secuencias = 0;
 
-        // Convertir la lista a mayúsculas para evitar conversiones en cada iteración
+        // Convertir la lista a mayúsculas
         for (int i = 0; i < n; i++) {
             adn.set(i, adn.get(i).toUpperCase());
         }
 
+        // Verifica las secuencias
+        secuencias += verHorizontal(adn, n);
+        if (secuencias >= secuenciasMin) return true;
+
+        secuencias += verVertical(adn, n);
+        if (secuencias >= secuenciasMin) return true;
+
+        secuencias += verDiagonalDescendente(adn, n);
+        if (secuencias >= secuenciasMin) return true;
+
+        secuencias += verDiagonalAscendente(adn, n);
+        if (secuencias >= secuenciasMin) return true;
+
+        return false;
+    }
+
+    private static Integer verHorizontal(List<String> adn, Integer n) throws Exception {
+        int secuencias = 0;
+
         // Verifica secuencias horizontales
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - 3; j++) {
-                //compara j+1 y j+2 si coinciden compara el anterior y el posterior
                 if (adn.get(i).charAt(j + 1) == adn.get(i).charAt(j + 2)) {
                     if (adn.get(i).charAt(j + 1) == adn.get(i).charAt(j + 3) && adn.get(i).charAt(j + 1) == adn.get(i).charAt(j)) {
-                        //revisa que no sea una secuencia ya contada
                         if (j != 0) {
                             if (adn.get(i).charAt(j) != adn.get(i).charAt(j - 1)) {
                                 secuencias++;
@@ -67,14 +84,17 @@ public class MutantesService {
                 }
             }
         }
+        return secuencias;
+    }
+
+    private static Integer verVertical(List<String> adn, Integer n) throws Exception {
+        int secuencias = 0;
 
         // Verifica secuencias verticales
         for (int j = 0; j < n; j++) {
             for (int i = 0; i < n - 3; i++) {
-                // Compara i + 1 y i + 2 si coinciden, compara el anterior y el posterior
                 if (adn.get(i + 1).charAt(j) == adn.get(i + 2).charAt(j)) {
                     if (adn.get(i + 1).charAt(j) == adn.get(i + 3).charAt(j) && adn.get(i + 1).charAt(j) == adn.get(i).charAt(j)) {
-                        // Revisa que no sea una secuencia ya contada
                         if (i != 0) {
                             if (adn.get(i).charAt(j) != adn.get(i - 1).charAt(j)) {
                                 secuencias++;
@@ -90,35 +110,48 @@ public class MutantesService {
             }
         }
 
+        return secuencias;
+    }
+
+    private static Integer verDiagonalDescendente(List<String> adn, Integer n) throws Exception {
+        int secuencias = 0;
+        int contador;
         // Verifica secuencias diagonales descendentes
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 1; j++) {
-                int contador = 1; // Reiniciar contador para cada diagonal
+                contador = 1; // Reiniciar contador para cada diagonal
                 while (i + contador < n && j + contador < n &&
                         adn.get(i).charAt(j) == adn.get(i + contador).charAt(j + contador)) {
                     contador++;
-                    if (contador == secuenciaMinima) { // Solo incrementa si se alcanza la secuencia mínima
+                    if (contador == 4) { // Solo incrementa si se alcanza la secuencia mínima
                         secuencias++;
                     }
                 }
             }
         }
 
+        return secuencias;
+    }
+
+    private static Integer verDiagonalAscendente(List<String> adn, Integer n) throws Exception {
+        int secuencias = 0;
+        int contador;
         // Verifica secuencias diagonales ascendentes
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < n - 1; j++) {
-                int contador = 1; // Reiniciar contador para cada diagonal
+                contador = 1; // Reiniciar contador para cada diagonal
                 while (i - contador >= 0 && j + contador < n &&
                         adn.get(i).charAt(j) == adn.get(i - contador).charAt(j + contador)) {
                     contador++;
-                    if (contador == secuenciaMinima) { // Solo incrementa si se alcanza la secuencia mínima
+                    if (contador == 4) { // Solo incrementa si se alcanza la secuencia mínima
                         secuencias++;
                     }
                 }
             }
         }
 
-        return secuencias >= secuenciasMin;
+        return secuencias;
     }
+
 
 }
